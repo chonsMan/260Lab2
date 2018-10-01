35a744bfd7f754660867d9464cff2c807395db71
chonsMan <jesseharris314@gmail.com>
2018-09-30 22:50:28 -0700

    second commit

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -22,3 +22,27 @@ list::~list()
 }
 
 
+void list::add (char ch)
+{
+	//Set previous and current nodes to traverse
+	node * prev = NULL;
+	node * curr = head;
+
+	//Traverse the list
+	while(curr != NULL && curr->item < ch)
+	{
+		prev = curr;
+		curr = curr->next;
+	}
+
+	//Add data to list
+	node * newNode = new node;//create new node to contain ch
+	newNode->item = ch;//add it to the item
+	newNode->next = NULL;
+	newNode->next = curr;//don't enter while loop again
+	if(prev == NULL)
+		head = newNode;
+	else
+		prev->next = newNode;
+
+}
